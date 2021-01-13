8bffbb2797d1d1ce5ffabbf9cfd2a1277f15b99d
mbridak <michael.bridak@gmail.com>
2021-01-13 06:29:46 -0800

    Added an icon installer/uninstaller for Linux

---
+++ install_icon/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+xdg-icon-resource install --size 64 --context apps --mode user k6gte-wfdlogger.png k6gte-wfdlogger
+
+xdg-desktop-icon install k6gte-wfdlogger.desktop
+
+xdg-desktop-menu install k6gte-wfdlogger.desktop
+
