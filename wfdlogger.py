47b07380a22143e119ab1f12720cb9b23d95d70c
mbridak <michael.bridak@gmail.com>
2021-10-14 09:21:01 -0700

    socket shutdown

--- wfdlogger.py
+++ wfdlogger.py
@@ -254,6 +254,7 @@ class MainWindow(QtWidgets.QMainWindow):
 				newfreq = self.rigctrlsocket.recv(1024).decode().strip()
 				self.rigctrlsocket.send(b'm\n')
 				newmode = self.rigctrlsocket.recv(1024).decode().strip().split()[0]
+				self.rigctrlsocket.shutdown(socket.SHUT_RDWR)
 				self.rigctrlsocket.close()
 				
 				self.radio_icon.setPixmap(QtGui.QPixmap(self.relpath('icon/radio_green.png')))
