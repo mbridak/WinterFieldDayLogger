98d951eb59bab4ade627296a09fa27c982b0b09d
mbridak <michael.bridak@gmail.com>
2022-05-03 08:47:02 -0700

    updated uninstaller to remove binary.

--- install_icon/uninstall.sh
+++ install_icon/uninstall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [ -f "~/.local/bin/wfdlogger" ]; then
+    rm ~/.local/bin/wfdlogger
+fi
+
 xdg-icon-resource uninstall --size 64 k6gte-wfdlogger
 
 xdg-desktop-icon uninstall k6gte-wfdlogger.desktop
